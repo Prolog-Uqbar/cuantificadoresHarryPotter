cc3335d3f7972129b86b98ec6d25890976107931
lspigariol <lspigariol@gmail.com>
2022-06-24 10:57:37 -0300

    hechos

---
+++ harrypotter.pl
@@ -0,0 +1,28 @@
+pelicula(1,"HP y la piedra filosofal").
+
+hechizo(inmobilus, facil).
+hechizo(avadaKedavra, imperdonable).
+hechizo(sectumSempra, dificil).
+
+pocion(multijugo,pelo,4).
+pocion(deshacerHechizo,mandragora,30).
+pocion(suerteLiquida,trebol,5).
+
+mago(harry).
+mago(hermione).
+mago(snape).
+
+objetoMagico(boletoHogwart).
+objetoMagico(varitaSauco).
+objetoMagico(capaInvisibilidad).
+
+usa(harry,capaInvisibilidad,3).
+usa(harry,capaInvisibilidad,1).
+usa(harry,inmobilus,1).
+usa(harry,avadaKedavra,7).
+usa(harry,multijugo,3).
+usa(snape,multijugo,5).
+usa(snape,inmobilus,5).
+usa(hermione,multijugo,3).
+usa(hermione,inmobilus,3).
+
