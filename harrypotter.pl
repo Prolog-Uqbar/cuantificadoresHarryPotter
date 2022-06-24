738c3a01898ae7eb4f36ed8ec5e0f84c94795867
lspigariol <lspigariol@gmail.com>
2022-06-24 12:52:25 -0300

    terminado

--- harrypotter.pl
+++ harrypotter.pl
@@ -15,7 +15,7 @@ pocion(suerteLiquida,trebol,5).
 mago(harry).
 mago(hermione).
 mago(snape).
-%mago(dumbledore).
+mago(dumbledore).
 
 
 usa(harry,capaInvisibilidad,3).
@@ -24,9 +24,11 @@ usa(harry,inmobilus,1).
 usa(harry,avadaKedavra,7).
 usa(harry,multijugo,3).
 usa(snape,multijugo,5).
-usa(snape,inmobilus,5).
+usa(snape,capaInvisibilidad,4).
+usa(snape,suerteLiquida,4).
+usa(snape,deshacerHechizo,4).
 usa(hermione,multijugo,3).
-usa(hermione,sectumSempra,3).
+%usa(hermione,sectumSempra,3).
 usa(luke,inmobilus,3).
 
 usaHechizo(Mago,Hechizo):-
@@ -49,11 +51,26 @@ hechizoImperdible(Hechizo):-
 
 hayUnMagoQueSeLoPierde(Hechizo):-
     mago(Mago),
-    not(usaHechizo(Mago,Hechizo)).
+    not(usa(Mago,Hechizo,_)).
 
 hechizoPopular(Hechizo):-
     hechizo(Hechizo,_),
-    forall(mago(Mago),usaHechizo(Mago,Hechizo)).
+    forall(mago(Mago),usa(Mago,Hechizo,_)).
 
-especialistaEnPociones1(). % solo usa Pociones
-especialistaEnPociones2(). % usa todas las Pociones
+
+soloUsaPociones1(Mago):-
+    mago(Mago), 
+    not(usaHechizo(Mago,_)), 
+    not((objetoMagico(ObjetoMagico),usa(Mago,ObjetoMagico,_))).
+
+soloUsaPociones2(Mago):-
+    mago(Mago), 
+    not((usa(Mago,Algo,_), not(pocion(Algo,_,_)))). 
+
+soloUsaPociones3(Mago):-
+    mago(Mago), 
+    forall(usa(Mago,Algo,_),pocion(Algo,_,_)).
+
+especialistaEnPociones(Mago):-
+    mago(Mago), 
+    forall(pocion(Algo,_,_), usa(Mago,Algo,_)).
